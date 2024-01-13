4e51aa595578c8d08e3605818d019d0001a8b582
riamems <membagloria@gmail.com>
2024-01-13 00:12:41 -0800

    updated work

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    Defines the class City.
+'''
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''
+        Defines the class City that inherits from BaseModel.
+    '''
+    state_id = ""
+    name = ""
